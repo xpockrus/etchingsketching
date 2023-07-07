415d1a180f39eeec38dafe43e5466b953cbef195
Alex Pockrus <xpockrus@gmail.com>
2023-07-06 19:59:48 -0700

    Complete random colors, couldn't do darkening

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const reset = document.querySelector("#newGrid");
+const newGrid = document.querySelector("#newGrid");
+const resetGrid = document.querySelector("#resetGrid");
+const randomBtn = document.querySelector("#random");
+const black = document.querySelector("#blackOut");
 
 function createGrid(size) {
     let container = document.querySelector("#container");
@@ -10,19 +13,74 @@ function createGrid(size) {
     let sizeSquared = size * size
     for (i = 0; i < sizeSquared; i++) {
         let square = document.createElement("div")
-        square.style.backgroundColor = "red"
+        square.classList.add("changed");
         container.appendChild(square);
     }
+    colorRed()
 }
 
 createGrid(16);
+function colorRed() {
+    let tiles = document.querySelectorAll(".changed");
+    tiles.forEach((tile) => {
+        tile.addEventListener("mouseover", colorSquare);
+    })
+}
 
-reset.addEventListener("click", () => {
+function colorSquare() {
+    this.style.backgroundColor = "red"
+}
+
+function randomInteger(max) {
+    return ~~(Math.random() * (max +1));
+}
+
+
+function randomColor() {
+    let tiles = document.querySelectorAll(".changed");
+    tiles.forEach((tile) => {
+        tile.addEventListener("mouseover", () => {
+            let r = randomInteger(255);
+            let g = randomInteger(255);
+            let b = randomInteger(255);
+            let randomColor = `rgb(${r}, ${g}, ${b})`;
+            tile.style.backgroundColor = randomColor;
+        })
+    })
+}
+
+function blackness() {
+    let tiles = document.querySelectorAll(".changed");
+    tiles.forEach((tile) => {
+        tile.addEventListener("mouseover", () => {
+            let num = 255;
+            num = (num - (num * 0.1));
+            let darkColor = `rgb(${num}, ${num}, ${num})`;
+            tile.style.backgroundColor = darkColor;
+        })
+    })
+}
+
+blackOut.addEventListener("click", () => {
+    blackness();
+});
+
+randomBtn.addEventListener("click", () => {
+    randomColor();
+});
+
+newGrid.addEventListener("click", () => {
     input = prompt("Enter a number, 2 to 100");
     if (input >= 2 && input <= 100) {
         createGrid(input);
     }
     else {
         alert("Can't you read ya wanker?");
+        console.log("ERROR");
     }
+});
+
+resetGrid.addEventListener("click", () => {
+    createGrid(16);
+    colorRed();
 });
